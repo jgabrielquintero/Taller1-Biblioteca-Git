bbbecfddc63f23b3e8c5a550b4fac100688a33b2
José Gabriel Quintero Ortiz <jgabrielquintero@unicesar.edu.co>
2026-08-17 14:51:51 -0500

    feat: crear clase Cliente heredando de Persona

--- src/main/java/com/mycompany/biblioteca/Persona.java
+++ src/main/java/com/mycompany/biblioteca/Persona.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author quint
- */
 public class Persona {
+    protected String id;
+    protected String nombre;
+    protected String telefono;
     
+    public Persona(String id, String nombre, String telefono) {
+    this.id = id;
+    this.nombre = nombre;
+    this.telefono = telefono;
+}
+    public String getNombre() {
+    return nombre;}
+    public String getTelefono() {
+    return telefono;}
+    public String getId() {
+    return id;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;}
+public void setTelefono(String telefono) {
+    this.telefono = telefono;  }  
+public void setId(String id) {
+    this.id = id;    
+}
 }
